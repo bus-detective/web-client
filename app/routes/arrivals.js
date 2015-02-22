79eb48b0fe2e4fdb152a29daf92a4ed6ae67626d
Mitch Lloyd and Kevin Rockwood <dev+mitchlloyd+rockwood@gaslightsoftware.com>
2015-02-22 14:39:06 -0500

    Use router to get arrivals by stop id

--- app/routes/arrivals.js
+++ app/routes/arrivals.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import request from 'ic-ajax';
 
 export default Ember.Route.extend({
-  model: function() {
-    return request('/api/arrivals/WAL7THi');
+  model: function(params) {
+    return request(`/api/arrivals/${params.stop_id}`);
   }
 });
