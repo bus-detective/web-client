783396c7ae868e25c13c4516279c888bc7e17bd8
Mitch Lloyd and Kevin Rockwood <dev+mitchlloyd+rockwood@gaslightsoftware.com>
2015-02-22 14:32:27 -0500

    Update with latest API and sort arrivals

---
+++ app/controllers/index.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+var computed = Ember.computed;
+
+export default Ember.Controller.extend({
+  arrivalSorting: ['time:asc'],
+  arrivals: computed.sort('model.arrivals', 'arrivalSorting')
+})
--- app/helpers/to-time.js
+++
@@ -1,7 +0,0 @@
-import Ember from 'ember';
-
-export function toTime(input) {
-  return new Date(input * 1000);
-}
-
-export default Ember.Handlebars.makeBoundHelper(toTime);
