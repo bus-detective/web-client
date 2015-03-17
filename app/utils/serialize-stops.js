4a74436004d000d89146409648f8c79e1d8aab61
Kevin Rockwood <kevin@rockwood.me>
2015-03-17 16:56:27 -0400

    Fix stop name deserialization

--- app/utils/serialize-stops.js
+++ app/utils/serialize-stops.js
@@ -2,7 +2,7 @@ export default function serializeStops(response) {
   return response.stops.map(function(stop) {
     return {
       id: stop.stop_id,
-      name: stop.stop_name
+      name: stop.name
     };
   });
 }
