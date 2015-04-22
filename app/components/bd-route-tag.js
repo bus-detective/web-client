3b81fd8e204e70b28db622ebb9757a6f830ba4d6
Ryan Arthur <ryan@gaslight.co>
2015-04-22 13:47:03 -0400

    Show route tags for each arrival at a stop

--- app/components/bd-route-tag.js
+++ app/components/bd-route-tag.js
@@ -1,10 +1,12 @@
 import Ember from 'ember';
+import stringToHue from 'bus-detective/utils/string-to-hue';
 
 export default Ember.Component.extend({
   attributeBindings: ['style'],
   tagName: 'span',
   classNames: ['tag'],
-  style: Ember.computed('route.id', function() {
-    return `background-color: #${this.get('route.color')}`;
+  style: Ember.computed('route.route_id', function() {
+    var hue = stringToHue(this.get('route.route_id').toString());
+    return `background-color: hsl(${hue}, 50%, 55%);`;
   })
 });
