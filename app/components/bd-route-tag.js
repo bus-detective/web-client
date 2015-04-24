99932ab1bf2882d793071ab2eb125b4f2b3f5377
Kevin Rockwood <kevin@rockwood.me>
2015-04-24 20:41:47 +0900

    Update route-tag with text color and short_name

--- app/components/bd-route-tag.js
+++ app/components/bd-route-tag.js
@@ -5,8 +5,7 @@ export default Ember.Component.extend({
   attributeBindings: ['style'],
   tagName: 'span',
   classNames: ['tag'],
-  style: Ember.computed('route.route_id', function() {
-    var hue = stringToHue(this.get('route.route_id').toString());
-    return `background-color: hsl(${hue}, 50%, 55%);`;
+  style: Ember.computed('route.id', function() {
+    return `background-color: #${this.get('route.color')}; color: #${this.get('route.text_color')}`;
   })
 });
