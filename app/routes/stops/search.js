b4c6936b0920734b369ad14e9703b87609b04e43
Kevin Rockwood <kevin@rockwood.me>
2015-05-30 08:41:25 -0400

    Hydrate the query url param from the route
    Previously we bound the query param directly to the searchQuery service
    value. Ember was setting this to null for some reason. Manually setting
    the value from within the route model hook fixes the issue.

--- app/routes/stops/search.js
+++ app/routes/stops/search.js
@@ -1,7 +1,9 @@
 import Ember from 'ember';
 import { searchStops } from 'bus-detective/utils/api';
+let inject = Ember.inject;
 
 export default Ember.Route.extend({
+  searchQuery: inject.service(),
   queryParams: {
     query: {
       refreshModel: true
@@ -9,6 +11,10 @@ export default Ember.Route.extend({
   },
 
   model: function(params) {
+    // Hydrate the searchQuery service if the user comes in via url
+    if (params.query !== this.get('searchQuery.value')) {
+      this.set('searchQuery.value', params.query);
+    }
     return searchStops(params);
   }
 });
