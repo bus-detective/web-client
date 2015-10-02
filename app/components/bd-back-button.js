bc70a823b21e01495523b41c69d399983395fd56
Michael Guterl <michael@diminishing.org>
2015-10-02 11:52:34 -0400

    Remove duplicate action name
    Thanks @amk221 in Ember Slack for the help.

--- app/components/bd-back-button.js
+++ app/components/bd-back-button.js
@@ -4,6 +4,7 @@ export default Ember.Component.extend({
   tagName: 'a',
   classNames: ['button button--return'],
   attributeBindings: ['href', 'title'],
+  action: 'goBack',
 
   click: function() {
     this.sendAction();
