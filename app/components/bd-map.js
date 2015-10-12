2efc7ad0f49a19d63bd42c25f137073e4f00a22e
Chris Moore <chrism@gaslight.co>
2015-10-12 14:06:03 -0400

    Migrate away from ember-leaflet in favor of leaflet
    We weren’t using any of the helpers and they were throwing most of the
    deprecation warnings.

--- app/components/bd-map.js
+++ app/components/bd-map.js
@@ -1,4 +1,5 @@
-import Leaflet from 'bus-detective/utils/leaflet';
+/* global L */
+
 import Ember from 'ember';
 let { run } = Ember;
 const TILE_URL = 'http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png';
@@ -38,21 +39,21 @@ export default Ember.Component.extend({
 
   configureMap() {
     let center = [this.get('lat'), this.get('lng')];
-    this.set('map', Leaflet.map(this.get('element'), this.get('mapOptions')));
+    this.set('map', L.map(this.get('element'), this.get('mapOptions')));
     this.get('map').setView(center, this.get('zoom'));
-    this.get('map').addLayer(Leaflet.tileLayer(TILE_URL, { detectRetina: true}));
-    this.set('shapeLayer', Leaflet.layerGroup().addTo(this.get('map')));
-    Leaflet.marker(center).addTo(this.get('map'));
+    this.get('map').addLayer(L.tileLayer(TILE_URL, { detectRetina: true}));
+    this.set('shapeLayer', L.layerGroup().addTo(this.get('map')));
+    L.marker(center).addTo(this.get('map'));
   },
 
   drawShapes() {
     this.get('shapeLayer').clearLayers();
 
     let shapes = this.get('shapes').map((shape) => {
-      return Leaflet.polyline(shape.get('coordinates'), { color: `#${shape.get('color')}` });
+      return L.polyline(shape.get('coordinates'), { color: `#${shape.get('color')}` });
     });
 
-    this.get('shapeLayer').addLayer(Leaflet.layerGroup(shapes));
+    this.get('shapeLayer').addLayer(L.layerGroup(shapes));
   },
 
   actions: {
