d7a29ca7db024861ea089f201617278097d1338d
Alex Padgett <padgett.alex@gmail.com>
2016-07-25 15:09:52 -0400

    Fix unit tests for history

--- tests/unit/services/history-test.js
+++ tests/unit/services/history-test.js
@@ -9,7 +9,17 @@ test('captures a transition', function(assert) {
   var secondTransition = { name: "second" };
 
   history.capture(firstTransition);
-  assert.equal(history.get('previous'), undefined);
   history.capture(secondTransition);
   assert.equal(history.get('previous'), firstTransition);
+});
+
+test('returns null when no history', function(assert) {
+  var history = this.subject();
+  history.clear();
+  var transition = { name: "first" };
+
+  history.capture(transition);
+  console.log(history);
+  let previous = history.get('previous');
+  assert.equal(previous, null);
 });
