47bdd78e97f1c1157181639265715b8e5b5dadf0
Katherine Tornwall <ktornwall@gmail.com>
2016-07-29 08:40:13 -0400

    Add reorderability to favorites
    Use the handle to reorder favorites

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 const { computed } = Ember;
 
 export default Ember.Controller.extend({
-  topFavorites: computed('favoriteStops.all', function () {
+  topFavorites: computed('favoriteStops.all.[]', function () {
     return this.get('favoriteStops.all').slice(0,5);
   })
 });
